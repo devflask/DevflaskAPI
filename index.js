353e58f7c81c29641e65d940fd8592c272697093
dehys <arijan.nikoci@gmail.com>
2021-02-23 03:04:43 +0100

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello cunt!"+req.body);
+    res.send("Hello cunt!"+req.body.name);
 });
 
 app.listen('8072', () => {
